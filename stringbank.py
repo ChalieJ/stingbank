4f93bcfc9e94bfdaafdb2007c3e25449ad8b1b5d
cjung <horang17502@gmail.com>
2015-06-04 00:30:11 +0900

    add save & load function

--- stringbank.py
+++ stringbank.py
@@ -13,41 +13,55 @@ class Form(QtWidgets.QDialog):
         self.setWindowTitle('String Bank')
         self.setGeometry(100,100,250,100)
 
-        labGetString = QtWidgets.QLabel('Get String')
-        self.bt_copy = QtWidgets.QPushButton('copy', self)
+        # 1st layout [ label | button | button ] ==========
+        # add widget
+        labGetString   = QtWidgets.QLabel('Get String')
+        self.bt_copy   = QtWidgets.QPushButton('copy', self)
         self.bt_delete = QtWidgets.QPushButton('delete', self)
-        self.bt_copy.clicked.connect(self.slot_copy_clipboard_1)
+
+        # add button event
+        self.bt_copy.clicked.connect(self.slot_copy_clipboard)
+        self.bt_delete.clicked.connect(self.slot_delete_clipboard)
+
+        # make layout & set widget
         hboxDescriptionList = QtWidgets.QHBoxLayout()
         hboxDescriptionList.addWidget(labGetString)
         hboxDescriptionList.addWidget(self.bt_copy)
         hboxDescriptionList.addWidget(self.bt_delete)
 
-        labSetString = QtWidgets.QLabel('Set String')
-        hboxDescriptionSave= QtWidgets.QHBoxLayout()
-        hboxDescriptionSave.addWidget(labSetString)
-
+        # 2nd layout [ combobox ] ==========
         self.cbList = QtWidgets.QComboBox(self)
         self.cbList.activated.connect(self.slot_set_label)
         hboxList = QtWidgets.QHBoxLayout()
         hboxList.addWidget(self.cbList)
 
+        # 3rd layout [ label ] - show save string
+        labSetString = QtWidgets.QLabel('Set String')
+        hboxDescriptionSave= QtWidgets.QHBoxLayout()
+        hboxDescriptionSave.addWidget(labSetString)
+
+        # layout [ empty space ]
+        labTemp = QtWidgets.QLabel(' ')
+        hboxTemp = QtWidgets.QHBoxLayout()
+        hboxTemp.addWidget(labTemp)
+
+        # 4th layout [ label ]
         self.labString = QtWidgets.QLabel()
         hboxString = QtWidgets.QHBoxLayout()
         hboxString.addWidget(self.labString)
 
+        # 5th layout
         self.le = QtWidgets.QLineEdit(self)
         self.bt_save = QtWidgets.QPushButton('save', self)
-        self.bt_save.clicked.connect(self.slot_save_clipboard_2)
+        self.bt_save.clicked.connect(self.slot_save_clipboard)
         hboxSave = QtWidgets.QHBoxLayout()
         hboxSave.addWidget(self.le)
         hboxSave.addWidget(self.bt_save)
 
-        labTemp = QtWidgets.QLabel('  ')
-        hboxTemp = QtWidgets.QHBoxLayout()
-        hboxTemp.addWidget(labTemp)
-
+        # set textedit
         self.te = QtWidgets.QTextEdit(self)
- 
+
+        # add hboxlay out to a vertical layout 
         vbox = QtWidgets.QVBoxLayout()
         vbox.addLayout(hboxDescriptionList)
         vbox.addLayout(hboxList)
@@ -56,23 +70,76 @@ class Form(QtWidgets.QDialog):
         vbox.addLayout(hboxDescriptionSave)
         vbox.addLayout(hboxSave)
         vbox.addWidget(self.te)
- 
+
+        # set layout
         self.setLayout(vbox)
- 
+
+        self.data_load()
+
+        # set clipboard...
         self.qclip = QtWidgets.QApplication.clipboard()
- 
+
+    # copy button event
     @pyqtSlot()
-    def slot_copy_clipboard_1(self):
+    def slot_copy_clipboard(self):
         self.qclip.setText(data[self.cbList.currentText()])
- 
+
+    # save button event
     @pyqtSlot()
-    def slot_save_clipboard_2(self):
+    def slot_save_clipboard(self):
+        # add item(combobox) & data structure
         self.cbList.addItem(self.le.text())
         data[self.le.text()] = self.te.toPlainText()
-        self.labString.setText(data[self.cbList.currentText()])
+	    # refresh item
+        self.refresh_item()	
 
+		# save file
+        self.data_save()	
+
+    # delete button event
+    @pyqtSlot()
+    def slot_delete_clipboard(self):
+        # delete structure
+        del data[self.cbList.currentText()]
+        # delete combobox item - current item
+        self.cbList.removeItem(self.cbList.findText(self.cbList.currentText()))
+        # refresh item
+        self.refresh_item()	
+	
+	    # save file
+        self.data_save()	
+
+    # combobox event
     @pyqtSlot()
     def slot_set_label(self):
+        self.labString.setText(data[self.cbList.currentText()])
+
+    def data_save(self):
+        fd = open("data.dat", "w")
+
+        for (key, value) in data.items():
+            fd.write(key + '|' + value + '\n')
+
+        fd.close()
+
+    def data_load(self):
+        fd = open("data.dat", "r")
+
+        line = fd.readline()
+
+        while line:
+            splittext = line.split('|')
+            print(splittext[0])
+            print(splittext[1])
+            self.cbList.addItem(splittext[0])
+            data[splittext[0]] = splittext[1]
+            self.labString.setText(splittext[1])
+            line = fd.readline()
+
+        fd.close()
+        self.refresh_item()	
+
+    def refresh_item(self):
         self.labString.setText(data[self.cbList.currentText()])
 
 
