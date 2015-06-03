c52b7ef85a2cf948f98c0eac749f52298bddc448
cjung <horang17502@gmail.com>
2015-06-03 22:59:37 +0900

    chnage UI

--- stringbank.py
+++ stringbank.py
@@ -10,33 +10,51 @@ data = {}
 class Form(QtWidgets.QDialog):
     def __init__(self, parent=None):
         QtWidgets.QDialog.__init__(self, parent)
+        self.setWindowTitle('String Bank')
         self.setGeometry(100,100,250,100)
- 
-        self.cb = QtWidgets.QComboBox(self)
-        self.cb.activated.connect(self.slot_set_label)
-        self.bt_1 = QtWidgets.QPushButton('copy', self)
-        self.bt_1.clicked.connect(self.slot_copy_clipboard_1)
-        hbox_1 = QtWidgets.QHBoxLayout()
-        hbox_1.addWidget(self.cb)
-        hbox_1.addWidget(self.bt_1)
+
+        labGetString = QtWidgets.QLabel('Get String')
+        self.bt_copy = QtWidgets.QPushButton('copy', self)
+        self.bt_delete = QtWidgets.QPushButton('delete', self)
+        self.bt_copy.clicked.connect(self.slot_copy_clipboard_1)
+        hboxDescriptionList = QtWidgets.QHBoxLayout()
+        hboxDescriptionList.addWidget(labGetString)
+        hboxDescriptionList.addWidget(self.bt_copy)
+        hboxDescriptionList.addWidget(self.bt_delete)
+
+        labSetString = QtWidgets.QLabel('Set String')
+        hboxDescriptionSave= QtWidgets.QHBoxLayout()
+        hboxDescriptionSave.addWidget(labSetString)
+
+        self.cbList = QtWidgets.QComboBox(self)
+        self.cbList.activated.connect(self.slot_set_label)
+        hboxList = QtWidgets.QHBoxLayout()
+        hboxList.addWidget(self.cbList)
 
         self.labString = QtWidgets.QLabel()
-        hbox_2 = QtWidgets.QHBoxLayout()
-        hbox_2.addWidget(self.labString)
+        hboxString = QtWidgets.QHBoxLayout()
+        hboxString.addWidget(self.labString)
 
         self.le = QtWidgets.QLineEdit(self)
-        self.bt_2 = QtWidgets.QPushButton('save', self)
-        self.bt_2.clicked.connect(self.slot_save_clipboard_2)
-        hbox_3 = QtWidgets.QHBoxLayout()
-        hbox_3.addWidget(self.le)
-        hbox_3.addWidget(self.bt_2)
- 
+        self.bt_save = QtWidgets.QPushButton('save', self)
+        self.bt_save.clicked.connect(self.slot_save_clipboard_2)
+        hboxSave = QtWidgets.QHBoxLayout()
+        hboxSave.addWidget(self.le)
+        hboxSave.addWidget(self.bt_save)
+
+        labTemp = QtWidgets.QLabel('  ')
+        hboxTemp = QtWidgets.QHBoxLayout()
+        hboxTemp.addWidget(labTemp)
+
         self.te = QtWidgets.QTextEdit(self)
  
         vbox = QtWidgets.QVBoxLayout()
-        vbox.addLayout(hbox_1)
-        vbox.addLayout(hbox_2)
-        vbox.addLayout(hbox_3)
+        vbox.addLayout(hboxDescriptionList)
+        vbox.addLayout(hboxList)
+        vbox.addLayout(hboxString)
+        vbox.addLayout(hboxTemp)
+        vbox.addLayout(hboxDescriptionSave)
+        vbox.addLayout(hboxSave)
         vbox.addWidget(self.te)
  
         self.setLayout(vbox)
@@ -45,17 +63,17 @@ class Form(QtWidgets.QDialog):
  
     @pyqtSlot()
     def slot_copy_clipboard_1(self):
-        self.qclip.setText(data[self.cb.currentText()])
+        self.qclip.setText(data[self.cbList.currentText()])
  
     @pyqtSlot()
     def slot_save_clipboard_2(self):
-        self.cb.addItem(self.le.text())
+        self.cbList.addItem(self.le.text())
         data[self.le.text()] = self.te.toPlainText()
-        self.labString.setText(data[self.cb.currentText()])
+        self.labString.setText(data[self.cbList.currentText()])
 
     @pyqtSlot()
     def slot_set_label(self):
-        self.labString.setText(data[self.cb.currentText()])
+        self.labString.setText(data[self.cbList.currentText()])
 
 
 if __name__ == '__main__':
