bc44d27175e73a66a17ee85ab43344e81327b9ea
ChalieJ <horang17502@gmail.com>
2015-06-04 14:19:05 +0900

    change execute base win32gui

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ from cx_Freeze import setup, Executable
 
 setup(  name = "stringbank",
         version = "0.1",
+        compress = True,
         description = "release version 0.1",
-        executables = [Executable("stringbank.py")])
-        #options = {"build_exe": build_exe_options},
-        #executables = [Executable("stringbank.py", base=base)])
-
+        executables = [Executable("stringbank.py", base = "Win32GUI")],
+		)
